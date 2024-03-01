8ca67ae41a816daadef2124c299e32171ffb0445
Kevin <kevind@debian.debian>
2024-03-02 11:20:43 +1300

    ...

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,13 @@
-const reactElement = React.createElement(
-"h1",
-    null,
-    "Hello world!!!"
-);
+class  HelloWorld extends React.Component {
+    render() {
+        return React.createElement(
+            "h1",
+            {"yourname":"Atilla"},
+            "Hey world!!!"
+        );  
+    }
+}
+const greeting = React.createElement(HelloWorld)
 const domNode = document.getElementById("root");
 const root = ReactDOM.createRoot(domNode);
-root.render(reactElement);
+root.render(greeting);
